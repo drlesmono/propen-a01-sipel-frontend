7b6c1d4fc59da45788f799791f43e414a1546eae
Dennis Lesmono <drlesmono627@gmail.com>
2021-06-04 17:01:12 +0700

    feat: added set period feature

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -42,13 +42,23 @@ class Dashboard extends Component {
             listPi: [],
             listMs: [],
             messageError: null,
-            listNamaBulan: [],
+            listNamaBulanPi: [],
+            listNamaBulanMs: [],
             piBelumSelesaiMsBelumSelesai: [0, 0],
             tepatWaktuTelat: [20, 10],
             piMasuk: [],
             piSelesai: [],
             msMasuk: [],
-            msSelesai: []
+            msSelesai: [],
+            setPeriodChart2: false,
+            startMonth2: "01_2021",
+            endMonth2: "12_2021",
+            setPeriodChart3: false,
+            startMonth3: "01_2021",
+            endMonth3: "12_2021",
+            setPeriodChart4: false,
+            startMonth4: "01_2021",
+            endMonth4: "12_2021",
         };
         this.handleEdit = this.handleEdit.bind(this);
         this.handleCancel = this.handleCancel.bind(this);
@@ -73,14 +83,19 @@ class Dashboard extends Component {
             const orders = await APIConfig.get("/ordersVerified/ms");
             const engineers = await APIConfig.get("/engineers");
             const listPi = await APIConfig.get("/orders/pi");
-            const listMs = await APIConfig.get("/orders/ms");
-            const listNamaBulan = await APIConfig.get("/orders/pi/namaBulan/01_2021/05_2021");
-            const piMasuk = await APIConfig.get("/orders/pi/masuk/01_2021/05_2021");
-            const piSelesai = await APIConfig.get("/orders/pi/selesai/01_2021/05_2021");
-            console.log(listNamaBulan);
-            console.log(listPi);
-            this.setState({ ordersVerified: orders.data, engineers: engineers.data, listPi: listPi.data, listMs: listMs.data,
-            listNamaBulan: listNamaBulan.data, piMasuk: piMasuk.data, piSelesai: piSelesai.data});
+            // const listMs = await APIConfig.get("/orders/ms");
+            const listNamaBulanPi = await APIConfig.get("/orders/pi/namaBulan/01_2021/12_2021");
+            const piMasuk = await APIConfig.get("/orders/pi/masuk/01_2021/12_2021");
+            const piSelesai = await APIConfig.get("/orders/pi/selesai/01_2021/12_2021");
+            const listNamaBulanMs = await APIConfig.get("/orders/ms/namaBulan/01_2021/12_2021");
+            const msMasuk = await APIConfig.get("/orders/ms/masuk/01_2021/12_2021");
+            const msSelesai = await APIConfig.get("/orders/ms/selesai/01_2021/12_2021");
+            // console.log(listNamaBulan);
+            // console.log(listPi);
+            this.setState({ordersVerified: orders.data, engineers: engineers.data, listPi: listPi.data,
+                listNamaBulanPi: listNamaBulanPi.data, piMasuk: piMasuk.data, piSelesai: piSelesai.data,
+                listNamaBulanMs: listNamaBulanMs.data, msMasuk: msMasuk.data, msSelesai: msSelesai.data,
+            });
             this.filterTelatTepatWaktu(this.state.listPi);
         } catch (error) {
             this.setState({ isError: true });
@@ -90,11 +105,20 @@ class Dashboard extends Component {
 
     async loadDataAfterSetPeriod() {
         try {
-            const orders = await APIConfig.get("/ordersVerified/ms");
-            const engineers = await APIConfig.get("/engineers");
             const listPi = await APIConfig.get("/orders/pi");
-            const listMs = await APIConfig.get("/orders/ms");
-            this.setState({ ordersVerified: orders.data, engineers: engineers.data, listPi: listPi.data, listMs: listMs.data});
+            // const listMs = await APIConfig.get("/orders/ms");
+            const listNamaBulanPi = await APIConfig.get(`/orders/pi/namaBulan/${this.state.startMonth3}/${this.state.endMonth3}`);
+            const piMasuk = await APIConfig.get(`/orders/pi/masuk/${this.state.startMonth3}/${this.state.endMonth3}`);
+            const piSelesai = await APIConfig.get(`/orders/pi/selesai/${this.state.startMonth3}/${this.state.endMonth3}`);
+            const listNamaBulanMs = await APIConfig.get(`/orders/ms/namaBulan/${this.state.startMonth4}/${this.state.endMonth4}`);
+            const msMasuk = await APIConfig.get(`/orders/ms/masuk/${this.state.startMonth4}/${this.state.endMonth4}`);
+            const msSelesai = await APIConfig.get(`/orders/ms/selesai/${this.state.startMonth4}/${this.state.endMonth4}`);
+            // console.log(listNamaBulan);
+            // console.log(listPi);
+            this.setState({listPi: listPi.data,
+                listNamaBulanPi: listNamaBulanPi.data, piMasuk: piMasuk.data, piSelesai: piSelesai.data,
+                listNamaBulanMs: listNamaBulanMs.data, msMasuk: msMasuk.data, msSelesai: msSelesai.data,
+            });
             this.filterTelatTepatWaktu(this.state.listPi);
         } catch (error) {
             this.setState({ isError: true });
@@ -302,20 +326,8 @@ class Dashboard extends Component {
     // Mengubah data yang ditargetkan sesuai dengan isi form
     handleChangeField(event) {
         const { name, value } = event.target;
-        const servicesEngineerNew = this.state.servicesEngineer;
-        const servicesEngineerNameNew = this.state.servicesEngineerName;
-
-        if( name.substring(0,16) === "servicesEngineer"){
-            let index = Number(name.substring(16));
-            servicesEngineerNew[index] = value;
-            this.setState({ servicesEngineer: servicesEngineerNew});
-        }else if( name.substring(0,11) === "serviceName" ){
-            let index = Number(name.substring(11));
-            servicesEngineerNameNew[index] = value;
-            this.setState({ servicesEngineerName: servicesEngineerNameNew});
-        }else{
-            this.setState({ [name]: value });
-        }
+        
+        this.setState({ [name]: value });
     }
 
     // Menargetkan order yang dipilih untuk diubah periode kontrak nya atau diperpanjang
@@ -350,6 +362,21 @@ class Dashboard extends Component {
         }
     }
 
+    // Memunculkan modal Set Period untuk Chart 2
+    handleSetPeriodChart2(event){
+        this.setState({setPeriodChart2: true})
+    }
+
+    // Memunculkan modal Set Period untuk Chart 3
+    handleSetPeriodChart3(event){
+        this.setState({setPeriodChart3: true})
+    }
+
+    // Memunculkan modal Set Period untuk Chart 4
+    handleSetPeriodChart4(event){
+        this.setState({setPeriodChart4: true})
+    }	
+
     // Menutup semua modal
     handleCancel(event) {
         event.preventDefault();
@@ -374,7 +401,16 @@ class Dashboard extends Component {
             isFailed: false,
             isSuccess: false,
             isError: false,
-            messageError: null
+            messageError: null,
+            setPeriodChart2: false,
+            startMonth2: null,
+            endMonth2: null,
+            setPeriodChart3: false,
+            startMonth3: null,
+            endMonth3: null,
+            setPeriodChart4: false,
+            startMonth4: null,
+            endMonth4: null,
         });
         this.loadData();
     }
@@ -548,10 +584,22 @@ class Dashboard extends Component {
             tepatWaktuTelat,
             piMasuk,
             piSelesai,
-            listNamaBulan
+            listNamaBulanPi,
+            lisNamaBulanMs,
+            msMasuk,
+            msSelesai,
+            setPeriodChart2,
+            startMonth2,
+            endMonth2,
+            setPeriodChart3,
+            startMonth3,
+            endMonth3,
+            setPeriodChart4,
+            startMonth4,
+            endMonth4
         } = this.state;
 
-        console.log(this.state.listNamaBulan);
+
         console.log(this.state.piMasuk);
         console.log(this.state.piSelesai);
         console.log(this.state.tepatWaktuTelat);
@@ -564,13 +612,15 @@ class Dashboard extends Component {
                     <div><h1 className="text-center">Dashboard</h1></div>
                     <Table>
                         <tr>
-                            <td></td>
+                            <td>
+                                <div><h1 className="text-center">Jumlah Project Installation dan Managed Services yang Belum Selesai</h1> </div>
+                            </td>
                             <td>
                                 <div><h1 className="text-center">Persentase Penyelesaian Tepat Waktu</h1></div>
                                 <DoughnutChart data={this.state.tepatWaktuTelat}></DoughnutChart>
                                 <Button
                                     className={classes.button2}
-                                    // onClick={() => this.handleFinalize(report)}
+                                    onClick={() => this.handleSetPeriodChart2()}
                                 >
                                     Set Period
                                 </Button>
@@ -579,28 +629,140 @@ class Dashboard extends Component {
                         <tr>
                             <td>
                                 <div><h1 className="text-center">Jumlah Project Installation yang Masuk dan Selesai</h1></div>
-                                <BarChart namaBulan={this.state.listNamaBulan} masuk={this.state.piMasuk} selesai={this.state.piSelesai}></BarChart>
+                                <BarChart namaBulan={this.state.listNamaBulanPi} masuk={this.state.piMasuk} selesai={this.state.piSelesai}></BarChart>
                                 <Button
                                     className={classes.button2}
-                                    // onClick={() => this.handleFinalize(report)}
+                                    onClick={() => this.handleSetPeriodChart3()}
                                 >
                                     Set Period
                                 </Button>
                             </td>
                             <td>
                                 <div><h1 className="text-center">Jumlah Managed Services yang Masuk dan Selesai</h1></div>
-                                <BarChart></BarChart>
+                                <BarChart namaBulan={this.state.listNamaBulanMs} masuk={this.state.msMasuk} selesai={this.state.msSelesai}></BarChart>
                                 <Button
                                     className={classes.button2}
-                                    // onClick={() => this.handleFinalize(report)}
+                                    onClick={() => this.handleSetPeriodChart4()}
                                 >
                                     Set Period
                                 </Button>
                             </td>
                         </tr>
                     </Table>
+                    <Modal
+                        show={setPeriodChart2}
+                        dialogClassName="modal-90w"
+                        aria-labelledby="contained-modal-title-vcenter"
+                    >
+                        <Modal.Header closeButton onClick={this.handleCancel}>
+                            <Modal.Title id="contained-modal-title-vcenter">
+                                Set Period
+                            </Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Form>
+                                <Table borderless responsive="xl" size="sm">
 
+                                    <tr>
+                                        <td>
+                                            <p>Start Month<p style={{color: "red"}}>*</p></p>
+                                        </td>
+                                        <td><Form.Control type="text" size="sm" name="startMonth2" className={classes.notes} onChange={this.handleChangeField} placeholder="e.g.: 01_2021"/></td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <p>End Month<p style={{color: "red"}}>*</p></p>
+                                        </td>
+                                        <td><Form.Control type="text" size="sm" name="endMonth2" className={classes.notes} onChange={this.handleChangeField} placeholder="e.g.: 12_2021"/></td>
+                                    </tr>
+                                    <tr>
+                                        <td style={{color: "red"}}>*Wajib diisi</td>
+                                        <td className="d-flex justify-content-end">
+                                            <Button variant="primary" className={classes.button1} onClick={this.handleCancel}>
+                                                Simpan
+                                            </Button>
+                                        </td>
+                                    </tr>
+                                </Table>
+                            </Form>
+                        </Modal.Body>
+                    </Modal>
+                    <Modal
+                        show={setPeriodChart3}
+                        dialogClassName="modal-90w"
+                        aria-labelledby="contained-modal-title-vcenter"
+                    >
+                        <Modal.Header closeButton onClick={this.handleCancel}>
+                            <Modal.Title id="contained-modal-title-vcenter">
+                                Set Period
+                            </Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Form>
+                                <Table borderless responsive="xl" size="sm">
 
+                                    <tr>
+                                        <td>
+                                            <p>Start Month<p style={{color: "red"}}>*</p></p>
+                                        </td>
+                                        <td><Form.Control type="text" size="sm" name="startMonth3" className={classes.notes} onChange={this.handleChangeField} placeholder="e.g.: 01_2021"/></td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <p>End Month<p style={{color: "red"}}>*</p></p>
+                                        </td>
+                                        <td><Form.Control type="text" size="sm" name="endMonth3" className={classes.notes} onChange={this.handleChangeField} placeholder="e.g.: 12_2021"/></td>
+                                    </tr>
+                                    <tr>
+                                        <td style={{color: "red"}}>*Wajib diisi</td>
+                                        <td className="d-flex justify-content-end">
+                                            <Button variant="primary" className={classes.button1} onClick={this.handleCancel}>
+                                                Simpan
+                                            </Button>
+                                        </td>
+                                    </tr>
+                                </Table>
+                            </Form>
+                        </Modal.Body>
+                    </Modal>
+                    <Modal
+                        show={setPeriodChart4}
+                        dialogClassName="modal-90w"
+                        aria-labelledby="contained-modal-title-vcenter"
+                    >
+                        <Modal.Header closeButton onClick={this.handleCancel}>
+                            <Modal.Title id="contained-modal-title-vcenter">
+                                Set Period
+                            </Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Form>
+                                <Table borderless responsive="xl" size="sm">
+
+                                    <tr>
+                                        <td>
+                                            <p>Start Month<p style={{color: "red"}}>*</p></p>
+                                        </td>
+                                        <td><Form.Control type="text" size="sm" name="startMonth4" className={classes.notes} onChange={this.handleChangeField} placeholder="e.g.: 01_2021"/></td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <p>End Month<p style={{color: "red"}}>*</p></p>
+                                        </td>
+                                        <td><Form.Control type="text" size="sm" name="endMonth4" className={classes.notes} onChange={this.handleChangeField} placeholder="e.g.: 12_2021"/></td>
+                                    </tr>
+                                    <tr>
+                                        <td style={{color: "red"}}>*Wajib diisi</td>
+                                        <td className="d-flex justify-content-end">
+                                            <Button variant="primary" className={classes.button1} onClick={this.handleCancel}>
+                                                Simpan
+                                            </Button>
+                                        </td>
+                                    </tr>
+                                </Table>
+                            </Form>
+                        </Modal.Body>
+                    </Modal>
                 </div>
             </>
 

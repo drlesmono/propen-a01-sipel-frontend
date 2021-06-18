cb976898bd11edbef08bb0073115a465df1e1d56
Dennis Lesmono <drlesmono627@gmail.com>
2021-06-18 23:19:11 +0700

    changed local urls in frontend services to localhost

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const API_URL = "https://propen-a01-sipel.herokuapp.com/api/auth/";
-const API_URL = "https://propen-a01-local.herokuapp.com/api/auth/";
+const API_URL = "http://localhost:2020/api/auth/";
 
 class AuthService {
   login(username, password) {

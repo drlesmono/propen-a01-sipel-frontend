6e1b9d4bf333ccec652c681fd67e97542adb0374
hannajtm <hanna14jtm@gmail.com>
2021-08-25 10:00:40 +0700

    fixed sequence order

--- src/containers/UpdateSequence/index.js
+++ src/containers/UpdateSequence/index.js
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
 import APIConfig from "../../APIConfig";
-import CustomizedTables from "../../components/Table";
-import CustomizedButtons from "../../components/Button";
-import Modal from "../../components/Modal";
-import {Form, Card, Table, Button} from "react-bootstrap";
-import { Input, FormControlLabel } from "@material-ui/core";
+import Modal from "react-bootstrap/Modal";
+import {Form, Card, Button} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./style.css";
 import classes from "./styles.module.css";
 import authHeader from "../../services/auth-header";
@@ -15,11 +13,14 @@ class UpdateSequence extends Component {
         super(props);
         this.state = {
             sequence: "",
-            isFilled: false,
+            tempSequence: "",
+            isNotFilled: false,
+            isError: false,
+            isSuccess: false
         };
         this.handleFilled = this.handleFilled.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-
+        this.handleCancel = this.handleCancel.bind(this);
     }
 
     componentDidMount() {
@@ -28,58 +29,65 @@ class UpdateSequence extends Component {
 
     async loadData() {
         try {
-            const seq = await APIConfig.get("/order/resetSeq", { headers: authHeader() });
-            this.state = {
-                sequence: seq.data,
-            };
+            const seq = await APIConfig.get("/order/sequence", { headers: authHeader() });
+            this.setState({ sequence: seq.data.sequenceValue });
         } catch (error) {
-            alert("Oops terjadi masalah pada server");
+            // alert("Oops terjadi masalah pada server");
+            this.setState({ isError: true });
             console.log(error);
         }
     }
 
     handleFilled(event){
         const { value } = event.target;
-        const { value2 } = this.state.sequence;
-
-        if(value === ""){
-            this.setState({
-                isFilled : false,
-                sequence:"" });
-        }else{
-            const tempSeq = {
-                sequenceNum: value,
-            };
-            this.setState({
-                isFilled : true,
-                sequence : tempSeq
-            });
-        }
+        this.setState({ tempSequence : value });
     }
-    handleSubmit(event){
-        //event.preventDefault();
-        let test = this.state.isFilled;
-        let val = this.state.sequence;
 
-        if(test === true){
+    async handleSubmit(event){
+        event.preventDefault();
+
+        const value = this.state.tempSequence;
+        const dataSeq = {
+            sequenceNum: value
+        }
+
+        if(value !== ""){
             try{
-                APIConfig.put(`/order/resetSeq/1`, val, { headers: authHeader() });
-
+                await APIConfig.put(`/order/resetSeq`, dataSeq, { headers: authHeader() });
             }catch (error) {
-                alert("Oops terjadi masalah pada server");
+                // alert("Oops terjadi masalah pada server");
+                this.setState({ isError: true });
                 console.log(error);
             }
+
+            this.loadData();
+            this.setState({ isSuccess: true, isError: false, isNotFilled: false });
         }
         else{
-            alert("Masukkan angka pada kolom yang tersedia!");
+            // alert("Masukkan angka pada kolom yang tersedia!");
+            this.setState({ isNotFilled: true });
         }
 
     }
 
+    // Menutup semua modal
+    handleCancel(event) {
+        event.preventDefault();
+
+        this.setState({
+            isError: false, 
+            isSuccess: false,
+            isNotFilled: false,
+            tempSequence: ""
+        });
+        this.loadData();
+    }
 
 
     render() {
-        let {isFilled, sequence} = this.state;
+        const {isNotFilled, sequence, isError, isSuccess} = this.state;
+        
+        const seqNotes = "Sequence order selanjutnya : " + sequence; 
 
         return(
             <div id="content">
@@ -87,11 +95,44 @@ class UpdateSequence extends Component {
                 <div>
                     <h1 class="text-center">Reset Urutan Nomor Order</h1>
                     <Card>
+                            <div>{seqNotes}</div><br></br><br></br>
                             <Form.Control type="number" className="sequence" size="sm" placeholder="Ubah nilai..." onChange={this.handleFilled}/>
-                            <Button className={classes.button4} onClick={() => this.handleSubmit()}>Update</Button>
+                            <Button className={classes.button4} onClick={this.handleSubmit}>Update</Button>
                     </Card>
                 </div>
             </div>
+
+                {/* Menampilkan modal berisi notifikasi ketika berhasil menyimpan data atau error */}
+                <Modal
+                    show={isSuccess || isError || isNotFilled}
+                    dialogClassName="modal-90w"
+                    aria-labelledby="contained-modal-title-vcenter"
+                >
+                     <Modal.Header closeButton onClick={this.handleCancel}>
+                        <Modal.Title id="contained-modal-title-vcenter">
+                            Notification
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        {isSuccess?
+                        <>
+                            <div className="d-flex justify-content-center">Sequence berhasil diubah menjadi {sequence}</div><br></br>
+                            <div className="d-flex justify-content-center">
+                                <Button variant="primary" className={classes.button1} onClick={this.handleCancel}>
+                                    Kembali
+                                </Button>
+                            </div>
+                        </> :
+                        <>
+                        <div className="d-flex justify-content-center"> {isError ? "Oops terjadi masalah pada server" : "Masukkan angka pada kolom yang tersedia!" }</div><br></br>
+                        <div className="d-flex justify-content-center">
+                            <Button variant="primary" className={classes.button1} onClick={this.handleCancel}>
+                                Kembali
+                            </Button>
+                        </div>
+                        </>}
+                    </Modal.Body>
+                </Modal>
             </div>
         );
     }
